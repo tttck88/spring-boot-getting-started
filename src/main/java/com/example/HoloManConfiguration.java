ac86da25e3c5a3c2fe89ba08dadbeb1d4b7e2097
taek <tttck88@gmail.com>
2022-02-07 21:07:28 +0900

    작업사항 - 자동설정 @ConfigurationProperties

--- src/main/java/com/example/HoloManConfiguration.java
+++ src/main/java/com/example/HoloManConfiguration.java
@@ -1,16 +1,20 @@
 package com.example;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
 public class HoloManConfiguration {
 
     @Bean
-    public Holoman holoman() {
+    @ConditionalOnMissingBean // 이 타입의 빈이 없을 경우에만 등록
+    public Holoman holoman(HolomanProperties holomanProperties) {
         Holoman holoman = new Holoman();
-        holoman.setHowLong(5);
-        holoman.setName("tttck");
+        holoman.setHowLong(holomanProperties.getHowLong());
+        holoman.setName(holomanProperties.getName());
         return holoman;
     }
 }
