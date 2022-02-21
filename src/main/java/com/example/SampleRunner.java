aa6131dc9f83bd0228735a97e11ec5d34b3d0113
taek <tttck88@gmail.com>
2022-02-21 18:40:38 +0900

    작업사항 - 외부설정 2부

--- src/main/java/com/example/SampleRunner.java
+++ src/main/java/com/example/SampleRunner.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -8,14 +8,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleRunner implements ApplicationRunner {
 
-    @Value("${tttck.name}")
-    private String name;
+//    @Value("${tttck.name}")
+//    private String name;
+//
+//    @Value("${tttck.age}")
+//    private String age;
 
-    @Value("${tttck.age}")
-    private String age;
+    @Autowired
+    TttckProperties tttckProperties;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("name");
+        System.out.println(tttckProperties.getName());
     }
 }
