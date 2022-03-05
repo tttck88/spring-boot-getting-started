d1bd6dec6420845225789165166615c6660995be
taek <tttck88@gmail.com>
2022-03-05 15:00:57 +0900

    작업사항 - 정적리소스 학습

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -9,7 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
-        app.setWebApplicationType(WebApplicationType.NONE);
         app.run(args);
     }
 }
