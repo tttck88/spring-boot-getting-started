f2cb4bed40587ccaaf17a0852e4cc27dde9e7934
taek <tttck88@gmail.com>
2022-06-23 23:19:24 +0900

    스프링 시큐리티 1부 : Starter-Security

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,11 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -18,15 +15,5 @@ public class Application {
     @Bean
     public WebClientCustomizer webClientCustomizer() {
         return webClientBuilder -> webClientBuilder.baseUrl("http://localhost:8080");
-    }
-
-    @Bean
-    public RestTemplateCustomizer restTemplateCustomizer() {
-        return new RestTemplateCustomizer() {
-            @Override
-            public void customize(RestTemplate restTemplate) {
-                restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-            }
-        };
     }
 }
---
+++ src/test/java/com/example/SampleControllerTest.java
@@ -0,0 +1,8 @@
+package com.example;
+
+import junit.framework.TestCase;
+
+public class SampleControllerTest {
+
+
+}
